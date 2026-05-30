3a6b1104273c8e7019b9e9dea6884b36b6f1d27d
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-05-30 19:31:50 -0400

    Integrate validation pipe globally and add DTO for message creation

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,25 +1,25 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
   @Get()
   listMessages() {
     return [
-      { id: 1, content: 'Hello'},
-      { id: 2, content: 'World!'},
+      { id: 1, content: 'Hello' },
+      { id: 2, content: 'World!' },
     ];
   }
 
   @Post()
-  createMessage(@Body() body: any) {
-    return { id: 3, content: body.content};
+  createMessage(@Body() body: CreateMessageDto) {
+    return { id: 3, content: body.content };
   }
 
   @Get(':id')
   getMessage(@Param('id') id: number) {
-
     console.log(`Retrieving message with ID: ${id}`);
 
-    return { id: id, content: 'Hello World'};
+    return { id: id, content: 'Hello World' };
   }
 }
