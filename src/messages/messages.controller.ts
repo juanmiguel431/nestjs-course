8aeeb2133b9c1f11e93ba2d5fee145a2b2426ce8
Juan Miguel Paulino Carpio <juanmiguel431@gmail.com>
2026-05-29 23:34:04 -0400

    Update MessagesController with basic CRUD methods and adjust ESLint config

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return [
+      { id: 1, content: 'Hello'},
+      { id: 2, content: 'World!'},
+    ];
+  }
+
+  @Post()
+  createMessage(@Body() body: any) {
+    return { id: 3, content: body.content};
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: number) {
+
+    console.log(`Retrieving message with ID: ${id}`);
+
+    return { id: id, content: 'Hello World'};
+  }
+}
